2f40c7e211481fc7fb4a3c9b2e5febee5b45cfaf
gouravgouravgupta <gouravgupta7879@gmail.com>
2021-12-05 14:17:32 +0530

    added all the files of project

---
+++ src/app/config/my-app-config.ts
@@ -0,0 +1,12 @@
+export default {
+
+    oidc: {
+        clientId: '0oa1k7f2dpy8wywRL5d7',
+        issuer: 'https://dev-29119566.okta.com/oauth2/default',
+        redirectUri: 'http://localhost:4200/login/callback',
+
+
+        scopes: ['openid', 'profile', 'email']
+    }
+
+}
